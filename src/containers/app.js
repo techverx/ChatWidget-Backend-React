941dc249ae6d89a454d8780fb9837902bc842fea
Dawar Aftab <dawar@gems.techverx.com>
2018-03-06 16:20:50 +0500

    awd

--- src/containers/app.js
+++ src/containers/app.js
@@ -1,26 +1,65 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import ChatInput from '../components/ChatInput';
 import ChatHistory from '../components/ChatHistory';
+import { setCurrentUserID, addMessage } from '../actions';
+import PubNub from 'pubnub'; 
+
+function mapStateToProps(state) {
+  return {
+    history: state.app.get('messages').toJS(),
+    userID: state.app.get('userID'),
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    addMessage: (message) => dispatch(addMessage(message)),
+    setUserID: (userID) => dispatch(setCurrentUserID(userID)),
+  };
+}
+
 class App extends React.Component {
-  state = {
-    userID: Math.round(Math.random() * 1000000).toString(),
-    history: [],
+  static propTypes = {
+    history: React.PropTypes.array,
+    userID: React.PropTypes.number,
+    addMessage: React.PropTypes.func,
+    setUserID: React.PropTypes.func,
   };
 
-  sendMessage = (message) => {
-    // for now this will let us know things work.  `console` will give us a
-    // warning though
-    console.log('sendMessage', message);
+  componentDidMount() {
+    const ID = Math.round(Math.random() * 1000000);
+    this.props.setUserID(ID);
+    this.PubNub = PUBNUB.init({
+      publish_key: 'pub-c-199f8cfb-5dd3-470f-baa7-d6cb52929ca4',
+      subscribe_key: 'sub-c-d2a5720a-1d1a-11e6-8b91-02ee2ddab7fe',
+      ssl: (location.protocol.toLowerCase() === 'https:'),
+    });
+    this.PubNub.subscribe({
+      channel: 'ReactChat',
+      message: this.props.addMessage,
+    });
   }
 
   render() {
+    const { props, sendMessage } = this;
     return (
       <div>
-        <ChatHistory history={ state.history } />
-        <ChatInput userID={ state.userID } sendMessage={ sendMessage } />
+        <ChatHistory history={ props.history } />
+        <ChatInput userID={ props.userID } sendMessage={ sendMessage } />
       </div>
     );
   }
+
+  sendMessage = (message) => {
+    this.PubNub.publish({
+      channel: 'ReactChat',
+      message: message,
+    });
+  }
 }
 
-export default App;
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(App);
